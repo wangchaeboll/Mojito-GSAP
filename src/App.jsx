abda209bf7a59c5e9c5bc9c61678067d89c1cccd
wangchaeboll <wang.chaeboll@gmail.com>
2025-07-07 11:20:03 +0800

    im not copying...

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import gsap from 'gsap'
+import {ScrollTrigger, SplitText} from "gsap/all";
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+
+gsap.registerPlugin(ScrollTrigger,SplitText);
+
+const App = () => {
+    return (
+        <main>
+            <Navbar/>
+            <Hero/>
+            <div className="h-dvh bg-black"></div>
+        </main>
+    )
+}
+export default App
